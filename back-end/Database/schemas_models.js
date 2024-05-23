ced1b1b1ff47f77e7b6fd8055f6779563855987f
James <j.smilezz052@gmail.com>
2024-05-23 16:10:13 +0100

    Updated User schema with displayName and location

--- back-end/Database/schemas_models.js
+++ back-end/Database/schemas_models.js
@@ -20,6 +20,10 @@ const locationSchema = new Schema({
   },
 });
 const UserSchema = new Schema({
+  displayName : {
+    type: String,
+    required: true
+  },
   username: {
     type: String,
     required: [true, "Please insert username"],
@@ -29,6 +33,10 @@ const UserSchema = new Schema({
     required: false,
     default:
       "https://upload.wikimedia.org/wikipedia/commons/a/af/Default_avatar_profile.jpg",
+  },
+  location : {
+    type: String,
+    require: true
   },
   settings: {
     searchRadius: Number,
--- back-end/index.js
+++ back-end/index.js
@@ -5,7 +5,7 @@ const { Location, User } = require("./seed.js");
 
 function connection() {
   return mongoose
-    .connect("mongodb://86.142.96.238:27017/DayPlanner-test")
+    .connect("mongodb://86.142.96.238:27017/DayPlanner")
     .then((result) => {
       return result;
     })
