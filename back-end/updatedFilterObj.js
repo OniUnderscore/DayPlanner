37662ff0f92b0a0c6ca00ba55bdb4605a9192af6
OniUnderscore <ellysgudger@gmail.com>
2024-05-23 11:04:22 +0100

    Get all sights from a location - including filtering

---
+++ back-end/updatedFilterObj.js
@@ -0,0 +1,38 @@
+const filter = {
+  amenity: {
+    arts_centre: true,
+    planetarium: true,
+    theatre: true,
+    marketplace: true,
+  },
+  historic: {
+    castle: true,
+    church: true,
+    fort: true,
+    manor: true,
+    monument: true,
+  },
+  leisure: {
+    amusement_arcade: true,
+    escape_game: true,
+    garden: true,
+    miniature_golf: true,
+    park: true,
+    nature_reserve: true,
+    stadium: true,
+    water_park: true,
+  },
+  shop: {
+    bakery: true,
+    coffee: true,
+  },
+  tourism: {
+    aquarium: true,
+    artwork: true,
+    attraction: true,
+    gallery: true,
+    museum: true,
+    theme_park: true,
+    zoo: true,
+  },
+};
